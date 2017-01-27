45403a0cbfd8b5250ab0fc607a6079ccbed88d9c
Froylan Camacho Tellez <Famachot@hotmail.com>
2017-01-26 23:59:58 -0600

    Se crea el proyecto y se agregan los modelos para las localidades en méxico

---
+++ app/mexico_bd/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MexicoBdConfig(AppConfig):
+    name = 'mexico_bd'
---
+++ app/mexico_bd/models.py
@@ -0,0 +1,59 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+
+
+class Estado(models.Model):
+	nombre = models.CharField(max_length=45)
+	class Meta:
+		ordering  = ('nombre',)
+		app_label = 'mexico_bd'
+	def __unicode__(self):
+		return self.nombre
+
+
+class Municipio(models.Model):
+	clave  = models.CharField(max_length=6)
+	estado = models.ForeignKey(Estado)
+	nombre = models.CharField(max_length=100)
+	class Meta:
+		ordering        = ('nombre',)
+		app_label       = 'mexico_bd'
+		unique_together = ('clave', 'estado',)
+	def __unicode__(self):
+		return self.nombre
+
+
+class TipoAsentadera(models.Model):
+	nombre = models.CharField(max_length=110)
+	class Meta:
+		ordering  = ('nombre',)
+		app_label = 'mexico_bd'
+	def __unicode__(self):
+		return self.nombre
+
+class Zona(models.Model):
+	nombre = models.CharField(max_length=110)
+	class Meta:
+		ordering  = ('nombre',)
+		app_label = 'mexico_bd'
+	def __unicode__(self):
+		return self.nombre
+
+class Asentadera(models.Model):
+	clave          = models.CharField(max_length=7)
+	tipo           = models.ForeignKey(TipoAsentadera)
+	nombre         = models.CharField(max_length=110)
+	cp  		   = models.CharField(max_length=7)	
+	zona           = models.ForeignKey(Zona)
+	municipio      = models.ForeignKey(Municipio,related_name='municipio')
+
+	class Meta:
+		ordering  = ('nombre',)
+		app_label = 'mexico_bd'
+	def __unicode__(self):
+		return self.nombre
